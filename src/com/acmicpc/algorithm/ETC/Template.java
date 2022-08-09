08bd8d8561b0c5db6f9d8ac12c4a4affdb57fa05
hrlim <gkgid0840@naver.com>
2022-08-10 00:23:09 +0900

    [ADD] CodingTest template

---
+++ src/com/acmicpc/algorithm/ETC/Template.java
@@ -0,0 +1,18 @@
+package com.acmicpc.algorithm.ETC;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Template {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    }
+
+    static int stringToInt(String s){
+        return Integer.parseInt(s);
+    }
+}
